24de25e4eaad296f4baa36ee9a0a455709668291
claurenza <claurenza@itba.edu.ar>
2024-07-02 15:50:59 -0300

    Update: changes in structure and approach of query2

--- query1.h
+++ query1.h
@@ -5,3 +5,6 @@ Query1ADT newQuery1();
 void addInfraction(Query1ADT query1, char * infraction, char * id);
 
 void freeQuery1(Query1ADT query1);
+
+// pending function, if not succesful, we will save the names of mostPopularID
+char * fetchName( size_t id);
--- query2.c
+++ query2.c
@@ -2,22 +2,17 @@
 #include <stdlib.h>
 #include <errno.h>
 #include "query2.h"
+#include "query1.h"
 
 typedef struct agencies{
-    char * agency;                  //name of the agency
-    TListInfraction first;          //list of infractions
-    struct agencies * tail;         //next agency
+    char * agencyName;                  //name of the agency
+    int * infractions;                  //array of appearances
+    size_t dim;                         //dimension of array
+    size_t mostPopularID;               // ID of most issued infraction of agency agencyName
+    struct agencies * tail;             //next agency
 }TAgencies;
 
 typedef struct agencies * TListAgency;
-
-typedef struct infractions{
-    char * infraction;              //name of the infraction
-    size_t count;                   //number of infractions
-    struct infractions * tail;      //next infraction
-}TInfraction;
-
-typedef struct infractions * TListInfraction;
 
 typedef struct Query2CDT{
     TListAgency first;                    //list of agencies
@@ -27,82 +22,58 @@ typedef struct Query2CDT{
 Query2ADT newQuery2(){
     return calloc(1,sizeof(Query2CDT));
 }
-//fijarme sutilezas de desempate por orden alfabetico, no contemple caso de que strcmp sea cero(ver eso)
+//changes in structure, as in execution time is not vible to do an "ordered list" and reorganize it
+//we are switching a list for an aparreance vector in which the one w the most appearances will be the one stored in the structure and exported
 
-static TListInfraction addInfractionRec(TListInfraction list,char * infraction, size_t totalFines){
-    int c;
-    if(list == NULL || totalFines > list->count || (totalFines == list->count && (c=strcmp(infraction, list->infraction) < 0)) ){
-        TListInfraction aux=malloc(sizeof(TInfraction));
-        errno=0;
-        if(aux == NULL || errno == ENOMEM){
-            return list;
+void addInfraction(TListAgency first,size_t infractionID){
+    if(infractionID > first->dim){
+        first->infractions=realloc(first->infractions,sizeof(int) * infractionID);
+        for(int i=first->dim; i < infractionID;i++){
+            first->infractions[i]=0;
         }
-        aux->infraction=malloc(sizeof(char) * (strlen(infraction) + 1));
-        // if memory not allocated succesfully, set errno and free
-        if(aux->infraction == NULL){
-            errno=ENOMEM;
-            free(aux);
-            return list;
-        }
-        strcpy(aux->infraction,infraction);
-        aux->count=totalFines;
-        return aux;
+        first->dim=infractionID;
     }
-    list->tail=addInfractionRec(list->tail,infraction,totalFines);
-    return list;
-}
-
-static TListAgency findAgency(TListAgency agencies, char * name) {
-    TListAgency aux = agencies;
-    while ( aux != NULL) {
-        int c = strcmp(aux->first, name);
-        if ( c == 0) {
-            return aux;
+    first->infractions[infractionID]++;
+    if(first->infractions[infractionID] > first->infractions[first->mostPopularID]){
+        first->mostPopularID=infractionID;
+    }else if(first->infractions[infractionID] == first->infractions[first->mostPopularID]){
+        if(strcmp(fetchName(first->infractions[infractionID]),fetchName(first->infractions[first->mostPopularID])) < 0){
+            first->mostPopularID=infractionID;
         }
-        if ( c > 0)
-            return NULL;
-        aux = aux->tail;
     }
-    return NULL;
 }
 
-void addInfraction(TListAgency agencies, char *issuingAgency,char * infraction, size_t totalFines){
-    TListAgency aux = findAgency(agencies, issuingAgency);
-    // not checking if I find the agency or not, since they are extracted from the csv
-    aux->first = addInfractionRec(aux->first,infraction,totalFines); // If it failed, errno will be left with a value other than zero
-}
-
-static TListAgency addAgencyRec(TListAgency first, char * name, int *ok){
+static TListAgency addAgencyRec(TListAgency first, char * name, int *ok,size_t infractionID){
     int d;
-    if ( first == NULL || ( d = strcmp(first->agency, name)) > 0) {
+    if ( first == NULL || ( d = strcmp(first->agencyName, name)) > 0) {
         errno = 0;
         TListAgency aux = malloc(sizeof(TAgencies));
         if ( aux==NULL || errno == ENOMEM) {
+            free(aux);
            return first;  // If an error ocurred, it returns the same list, without making changes
         }
-        aux->first= NULL;// ?
-        aux->agency= malloc(strlen(name)+1);
-        if(aux->agency == NULL){
+        aux->agencyName= malloc(strlen(name)+1);
+        if(aux->agencyName == NULL){
             errno=ENOMEM;
             free(aux);
             return first;
         }
-        strcpy(aux->agency, name);
+        strcpy(aux->agencyName, name);
         aux->tail = first;
+        addInfraction(aux,infractionID);
         *ok=1;
         return aux;
     }
     if ( d < 0){
-        first->tail = addAgencyRec(first->tail, name, ok);
+        first->tail = addAgencyRec(first->tail, name, ok,infractionID);
     }
     return first;
 }
 
-void addAgency(Query2ADT query, char * nameOfAgency) {
+void addAgency(Query2ADT query, char * nameOfAgency, size_t infractionID) {
     int ok=0;
-    query->first= addPersonRec(query->first, nameOfAgency, &ok);
+    query->first= addAgencyRec(query->first, nameOfAgency, &ok,infractionID);
     if(ok){
         query->size++;
     }
 }
-
